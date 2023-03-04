0e95dd3a65e44ab93a184f69f893de676e2a6354
Min Woo Sun <minwoosun55@gmail.com>
2023-03-03 21:22:03 -0800

    refactor code for colon data simulation like n100p10.R

--- experiments/colon.R
+++ experiments/colon.R
@@ -1,57 +1,98 @@
-start.time <- Sys.time()
-library(dplyr)
-library(glmnet)
-library(survival)
-library(groupdata2)
-require(doMC)
-registerDoMC(cores = 32)
-source("/home/users/minwoos/projects/ncv/code/ncv.R")
-source("/home/users/minwoos/projects/ncv/code/experiment.R")
-outdir = "/scratch/users/minwoos/ncv/output/"
-#############
-# load data #
-#############
+here::i_am("experiments/colon.R")
+
+library(here)
+source(here::here("R/ncv.R"))
+source(here::here("R/simulation.R"))
+
+# simulation parameters
+RUN_SIMULATION <- TRUE
+SIMULATION_SEED <- 123
+N_SIM <- 3
+ALPHA <- 0.10
+MC_CORES <- 3
+
+# data & ncv parameters
+N_REP <- 20
+N_FOLDS <- 10
+N_TRAIN <- 250
+
+# output parameters
+JOBNAME <- "colon"
+OUTPUT_DIR <- here::here("data/simulation_output/") # <- change this for sherlock
+
+
+############################
+# Load and preprocess data #
+############################
 data(cancer, package="survival")
-df = colon
-df = df[complete.cases(df),]
-df = df[df\$etype == 2,] # select death events
-df\$rx = df\$rx  %>%  as.character
-df\$rx[df\$rx == "Lev+5FU"] = 0
-df\$rx[df\$rx == "Lev"] = 1
-df\$rx[df\$rx == "Obs"] = 2
-df\$rx = df\$rx %>% as.integer
-x = df %>% select(-c(id, study, status, time, etype)) %>% data.matrix %>% as.matrix
-y = df %>% select(status, time) %>% data.matrix %>% as.matrix
-##################
-# run CV and NCV #
-##################
-run=T
-alpha=0.10
-nsim=200
-nreps=200
-nfolds=10
-ntrain=250
-ntest = dim(x)[1] - ntrain
-mc.cores=32
-if (run==T){
-  set.seed(888)
-  exp_result = experiment_real(x=x,
-                               y=y,
-                               nsim=nsim,
-                               ntrain=ntrain,
-                               nreps=nreps,
-                               nfolds=nfolds,
-                               alpha=alpha,
-                               mc.cores=mc.cores,
-                               verbose=T)
-  # assign output to respective variables
-  exp_result[[length(exp_result)+1]] = list(
-                           c("1: alpha ","2: ntrain", "3: ntest", "4: nsim","5: nreps","6: nfolds"),
-                           alpha,
-                           ntrain,
-                           ntest,
-                           nsim,
-                           nreps,
-                           nfolds)
+df <- colon
+
+# data preprocessing
+df <- df[complete.cases(df),]
+df <- df[df$etype == 2,] # select death events
+df$rx <- as.character(df$rx) 
+df$rx[df$rx == "Lev+5FU"] <- 0
+df$rx[df$rx == "Lev"] <- 1
+df$rx[df$rx == "Obs"] <- 2
+df$rx <- as.integer(df$rx)
+
+x <- df %>% 
+      select(-c(id, study, status, time, etype)) %>% 
+      data.matrix %>% 
+      as.matrix
+
+y <- df %>% 
+      select(status, time) %>% 
+      data.matrix %>% 
+      as.matrix
+
+
+###################################
+#  Create NSIM train-test splits  #
+###################################
+df_list <- list("x"=x, "y"=y)
+split_df_list <- split_train_test_nsim_times(df_list, N_TRAIN, N_SIM)
+data_train <- split_df_list[["train"]]
+data_test <- split_df_list[["test"]]
+
+
+#####################################
+#         Run simulation            #
+#####################################
+# Set RUN_SIMULATION to FALSE if already have simulation results,
+# and instead load the simulation results.
+# When a < b for MSE estimation, you will get NaN,
+# make NSIM larger to get desired NSIM as NaN will be discarded.
+if (RUN_SIMULATION) {
+  
+  registerDoMC(cores = MC_CORES)
+  set.seed(SIMULATION_SEED)
+  start.time <- Sys.time()
+  
+  simulation_result <- simulation(data_train, 
+                                  data_test, 
+                                  nsim=N_SIM, 
+                                  nreps=N_REP, 
+                                  nfolds=N_FOLDS, 
+                                  alpha=ALPHA, 
+                                  verbose=TRUE, 
+                                  mc.cores=MC_CORES
+                                  )
+  
+  # include simulation compute time in output
+  end.time <- Sys.time()
+  compute.time <- end.time - start.time
+  simulation_result$simulation_params$compute.time <- compute.time
+  
+  # save result
+  save(simulation_result, file=paste0(OUTPUT_DIR,JOBNAME,".RData"))
+  print(paste0("Saved simulation result: ",OUTPUT_DIR,JOBNAME,".RData"))
+  
+} else {
+  
+  # load result
+  load(file=paste0(OUTPUT_DIR,JOBNAME,".RData"))
 }
-save(exp_result, file=paste0(outdir,"colon3.RData"))
+
+
+
